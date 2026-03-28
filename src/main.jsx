c7f3a880ab41d444d402e0cd4ee6e96c27a08c25
Tommynissa <goubauxthomas@gmail.com>
2026-03-28 10:28:21 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>GestioLoc Airbnb</h1>
+      <p>Ça fonctionne 🚀</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
